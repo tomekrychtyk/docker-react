6a4811c6eaa069440686d39f45a9e54d388fb47f
Tomek Rychtyk <tomasz.rychtyk@crowdicity.com>
2020-03-10 23:47:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - YOYOYO!33
         </a>
       </header>
     </div>
